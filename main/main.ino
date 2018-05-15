2af0e9eeb8538cb6be2113483d0d1a28557519f3
Lodewijk <lodemichels@gmail.com>
2018-05-15 11:35:50 +0200

    bukker

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,15 @@
-#include <SPI.h>
+#define SSD1306_128_64
+#undef SSD1306_128_32
 #include <Wire.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#define OLED_ADDR   0x3C
 
-// If using software SPI (the default case):
-#define OLED_MOSI   9
-#define OLED_CLK   10
-#define OLED_DC    11
-#define OLED_CS    12
-#define OLED_RESET 13
-Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+Adafruit_SSD1306 display(-1);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
 
 #include <Adafruit_NeoPixel.h>
 #include <stdint.h>
@@ -17,66 +17,112 @@ Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 #include "avr/io.h"
 #include "util/delay.h"
 
-const byte analogPin = A0;
-//const byte ledPin =  13;
-const byte tonePin = 7;
-const byte interruptPin = 2;
-
-volatile uint8_t count = 0;
-
-void click(void);
-
-#ifndef abs
-   #define abs(x) (((x)<0) ? -(x) : (x))
-#endif
-
-uint8_t ping_pong(uint8_t min, uint8_t max, uint8_t offset){
-    uint8_t delta = (max - min);
-    return min + abs(((offset + delta) % (delta << 1)) - delta);
-}
+#define analogPin A0
+//const byte ledPin  13;
+#define tonePin 7
+#define interruptPin 19
 
 #define WS2812_PIN 6
 
+//volatile uint8_t count = 0;
+
+void click(void);
+
+//#ifndef abs
+//   #define abs(x) (((x)<0) ? -(x) : (x))
+//#endif
+
+//uint8_t ping_pong(uint8_t min, uint8_t max, uint8_t offset){
+//    uint8_t delta = (max - min);
+//    return min + abs(((offset + delta) % (delta << 1)) - delta);
+//}
+
+
+Adafruit_NeoPixel strip;
+
 void setup(void)
 {  
-  Serial.begin(9600);
-  Serial.print("init...");
-                
+  //    
   pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), click, RISING);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), click, FALLING);
 
-  // Parameter 1 = number of pixels in strip
-  // Parameter 2 = pin number (most are valid)
-  // Parameter 3 = pixel type flags, add together as needed:
-  //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-  //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-  //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-  //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-  Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, WS2812_PIN, NEO_GRB + NEO_KHZ400);
+  //
+  strip = Adafruit_NeoPixel(16, WS2812_PIN, NEO_GRB + NEO_KHZ800);
   strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
-  strip.setPixelColor(0, 0xff, 0xff, 0xff); //rgb
+  strip.show();
+
+  // initialize and clear display
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  display.display();
+
+  
+  // display a pixel in each corner of the screen
+  display.drawPixel(0, 0, WHITE);
+  display.drawPixel(127, 0, WHITE);
+  display.drawPixel(0, 63, WHITE);
+  display.drawPixel(127, 63, WHITE);
+
+  // display a line of text
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(27,30);
+  display.print("Hello, world!");
+
+  // update display with all of the above graphics
+  display.display();
+  
 }
+
+typedef enum{
+  state_idle,
+  state_game_1,
+} state_t;
+
+state_t state = state_idle;
+
+volatile uint8_t offset;
 
 void loop(void)                     
 {
-  Serial.println(ping_pong(0, 24, count));
-  count++;
-  
-  _delay_ms(125);
+  switch(state){
+    case state_idle:
+    
+      break;
+    case state_game_1:
+      offset++;
+      if(offset == 16){
+        offset = 0;
+      }
+      
+      uint8_t j;
+      for(j=0; j<16; j++){
+        strip.setPixelColor(j, 0, 0, 0);
+      }
+      strip.setPixelColor(0, 0, 255, 0);
+      strip.setPixelColor(offset, 255, 0, 0); 
+      strip.show();
+      _delay_ms(20);
+
+      break;
+  }
 }
 
-////
+//
 void click(void){
   _delay_ms(150);
 
-  Serial.print("click: ");
-  uint16_t adc = analogRead(analogPin);
+  switch(state){
+    case state_idle:
+       state = state_game_1;
+       break;
+    case state_game_1:
+       tone(tonePin, 1000, 200);
+       state = state_idle;
 
-  Serial.print(adc);
-  Serial.print(" ");
-
-  Serial.println(ping_pong(0, 24, count));
-
-  tone(tonePin, adc + 200, 500);
+       Serial.print("click: ");
+       Serial.println(offset);
+  
+       break;
+  }
 }
