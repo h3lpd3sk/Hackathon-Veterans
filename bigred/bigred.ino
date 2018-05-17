d2e805be811ab88c1fbd148149b8664b71956b8b
Lodewijk <lodemichels@gmail.com>
2018-05-17 15:37:57 +0200

    wipkip

--- bigred/bigred.ino
+++ bigred/bigred.ino
@@ -1,30 +1,60 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIXEL_PIN 10
+#define PIXEL_COUNT 8
+
+Adafruit_NeoPixel bar = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
 #define ISR_1 2
 
 volatile bool isr_changed = false;
+static uint8_t x;
+
+void neopixel_fill(Adafruit_NeoPixel* leds, uint8_t red, uint8_t green, uint8_t blue, uint8_t interval){
+  uint8_t i=0;
+  for(i=0; i < leds->numPixels(); i++){
+      leds->setPixelColor(i, red, green, blue);
+      leds->show(); 
+      delay(interval);
+  }  
+}
 
 void setup(void){
   pinMode(ISR_1, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ISR_1), isr, FALLING);
+  attachInterrupt(digitalPinToInterrupt(ISR_1), bigred_isr, FALLING);
 
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(9600);
+  Serial.println("init");
+
+  bar.begin();
+  bar.show();
+  
+  for(x=0; x < bar.numPixels(); x++){
+    bar.setPixelColor(x, 0, 0, 0);
+    bar.show(); 
+  }
 }
 void loop(void){
   if(isr_changed){
     isr_changed = false;
     
-    digitalWrite(LED_BUILTIN, HIGH);
-    _delay_ms(250);
-    digitalWrite(LED_BUILTIN, LOW);
-  }
+    neopixel_fill(&bar, 0, 0, 255, 100);
+    delay(1000);
+    neopixel_fill(&bar, 0, 0, 0, 0);
+   }
 }
-static void isr(void){
+static void bigred_isr(void){
+  static bool fired = false;
   noInterrupts();
   
-  //Big red
-  if(!digitalRead(ISR_1)){
+  if(fired == false && !digitalRead(ISR_1)){
     isr_changed = true;
+    fired = true;
+  }
+  else if(digitalRead(ISR_1)){
+    fired = false;
   }
   interrupts();
 }
